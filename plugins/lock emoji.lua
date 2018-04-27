68eca0e94861d70ded1b6378b189066ae5b1454c
TeleOled <38462703+OLEDTEAM@users.noreply.github.com>
2018-04-27 07:02:25 -0600

    Create lock emoji.lua

---
+++ plugins/lock emoji.lua
@@ -0,0 +1,54 @@
+local function run(msg)
+
+    local data = load_data(_config.moderation.data)
+
+     if data[tostring(msg.to.id)]['settings']['lock_emoji'] == 'yes' then
+
+
+if msg.to.type == 'channel' and not is_momod(msg) then
+	delete_msg(msg.id,ok_cb,false)
+	else
+	kick_user(msg.from.id, msg.to.id)
+
+        return 
+      end
+   end
+end
+
+return {patterns = {
+    "❤️",
+    "😂",
+    "😘",
+    "😍",
+    "😁",
+    "👍",
+    "😊",
+    "😔",
+    "☺️",
+    "😄",
+    "😭",
+    "💋",
+    "😒",
+    "😒",
+    "😜",
+    "🙈",
+    "😉",
+    "😃",
+    "😢",
+    "😝",
+    "😡",
+    "😱",
+    "😞",
+    "😏",
+    "😅",
+    "😚",
+    "🙊",
+    "😌",
+    "😐",
+    "😀",
+    "😋",
+    "😆",
+    "👌",
+    "☹️",
+    "😕",
+}, run = run}
